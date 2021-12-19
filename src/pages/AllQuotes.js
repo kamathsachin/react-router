ad8ef0a0fc64485a6f717f0b1069caa47c4bf02f
Sachin Kamath <kamathsachin@hotmail.com>
2021-12-19 15:26:52 -0600

    Implemented Linking for New Quote and Quote Details

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,19 @@
+import QuoteList from '../components/quotes/QuoteList';
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Sachin',
+    text: 'Learning React is Fun',
+  },
+  {
+    id: 'q2',
+    author: 'Shubha',
+    text: 'Learning JavaScript is Fun',
+  },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,32 @@
 import { useParams, Route } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: 'Sachin',
+    text: 'Learning React is Fun',
+  },
+  {
+    id: 'q2',
+    author: 'Shubha',
+    text: 'Learning JavaScript is Fun',
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No Quote Found</p>;
+  }
+
   return (
     <section>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
