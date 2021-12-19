585cbd0703f2544d05266f53167d4408d28f0f14
Sachin Kamath <kamathsachin@hotmail.com>
2021-12-19 14:16:37 -0600

    Added new folder structure for new implementation

--- README.md
+++
@@ -1,2 +0,0 @@
-# React Router Implementation
-- Installed 3rd Party Package React Router (npm install react-router-dom@5)
--- src/App.js
+++ src/App.js
@@ -1,24 +1,7 @@
-import { Route } from 'react-router-dom';
-import MainHeader from './components/MainHeader';
-import ProductDetail from './pages/ProductDetail';
-import Products from './pages/Products';
-import Welcome from './pages/Welcome';
-
 function App() {
   return (
     <div>
-      <MainHeader />
-      <main>
-        <Route path="/welcome">
-          <Welcome />
-        </Route>
-        <Route path="/products">
-          <Products />
-        </Route>
-        <Route path="/product-detail/:productId">
-          <ProductDetail />
-        </Route>
-      </main>
+
     </div>
   );
 }
